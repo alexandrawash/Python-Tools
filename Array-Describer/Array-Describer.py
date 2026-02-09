8c9ecf154381130bb887d24c2474352d0b8dfcfe
alexandra <167406220+alexandrawash@users.noreply.github.com>
2026-02-09 14:00:39 -0700

    Create Array-Describer.py

---
+++ Array-Describer/Array-Describer.py
@@ -0,0 +1,14 @@
+# First import necessary things
+import numpy as np
+
+# Array Describing Function:
+def describe(a):
+    '''Summarize the basic characteristics of an array.'''
+
+    print(f"number of dimensions is {a.ndim}")
+    print(f"shape is {a.shape}")
+    print(f"the minimum value is {a.min()}")
+    print(f"the maximum value is {a.max()}")
+    print(f"the standard deviation is {np.std(a)}") #using numpy standard deviation command
+    print(f"the 1st percentile is {np.percentile(a, 1)}")
+    print(f"the 99.9th percentile is {np.percentile(a, 99.9)}")
