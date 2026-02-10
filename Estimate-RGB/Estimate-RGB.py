bd45447e55ade7ef4c5b135cf85d0eb77ac08f5c
alexandra <167406220+alexandrawash@users.noreply.github.com>
2026-02-09 17:18:42 -0700

    Create Estimate-RGB.py

---
+++ Estimate-RGB/Estimate-RGB.py
@@ -0,0 +1,28 @@
+def estimateRGB(wavelengths, fluxes):
+    """
+    This function estimates the RGB color of an object, from its spectrum.
+    It calculates the integrals the flux over wavelength ranges
+    that correspond roughly to the red, green, and blue colors
+    displayed on computer monitors.
+    
+    Arguments:
+    wavelengths = a numpy array of wavelengths, in nanometers
+    fluxes = a numpy array of fluxes, corresponding to w
+    
+    Returns:
+    an RGB color, expressed as a three-element array,
+    with R as the first element, G the second, B the third
+
+    """
+    
+    #integrate the color areas
+    RedArea = integrateSpectrum(wavelengths, fluxes, 565, 660)
+    GreenArea = integrateSpectrum(wavelengths, fluxes, 485, 570)
+    BlueArea = integrateSpectrum(wavelengths, fluxes, 400, 490)
+    
+    #Put all of our RGB areas into a numpy array
+    RGB = np.array([RedArea, GreenArea, BlueArea])
+    
+    maximum = np.max(RGB)
+    
+    return RGB / maximum
